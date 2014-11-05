95325bccebf801c25a54134e0364ed33b82f69df
[D] Pakchoi <pakchoidora@gmail.com>
2014-11-05 15:39:13 +0800

    using node-webkit as client to help develope.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-"use strict"
+var gui = require('nw.gui');
+var win = gui.Window.get();
+var stage, ctx;
+$(document).ready(function() {
+	win.show();
+	stage = $('#stage').get(0);
+	ctx = stage.getContext('2d');
+	var temp = ctx.fillStyle;
+	ctx.fillStyle='#000';
+	ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+	ctx.fillStyle = temp;
+});
