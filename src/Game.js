b0050483e2a7de43c11964cbc70ff5727388fa6b
[D] Pakchoi <pakchoidora@gmail.com>
2014-12-25 14:39:12 +0800

    some thing conflict.

--- src/Game.js
+++ src/Game.js
@@ -1,32 +1,25 @@
 define(function(require, exports, module) {
-	var $ = require('jquery');
-	var Stage = require('awt/Stage');
-	var _;
-
+	var $ = require('jquery'),
+		Stage = require('awt/Stage'),
+		self;
 	function Game() {
-		_ = this;
-		_.stage = {};
-		_.graphics = {};
-		_.init();
-	}
-	Game.prototype.update = function () {
-		_.render();
-	};
-	Game.prototype.speed = 10000;
-	Game.prototype.render = function() {
-		$(_.stage.scene).attr({
-			width: _.stage.width(),
-			height: _.stage.height()
-		});
-	};
-	Game.prototype.init = function () {
+		self = this;
+		self.stage = {};
+		self.graphics = {};
 		$(window).on({
 			keypress: function(evt) {handleKeyEvent(evt);},
 			contextmenu: function(evt) {evt.preventDefault();}
 		});
-		_.stage = new Stage($('#stage'));
-		_.graphics = _.stage.graphics;
-		_.interval = setInterval(_.update, Game.speed);
+		self.stage = new Stage($('#stage'));
+		self.graphics = self.stage.graphics;
+		self.interval = setInterval(self.update, self.speed);
+	}
+	Game.prototype.update = function () {
+		self.render();
+	};
+	Game.prototype.speed = 50;
+	Game.prototype.render = function() {
+		self.stage.clear();
 	};
 	function handleKeyEvent(evt) {
 		switch(evt.keyCode) {
--- src/index.js
+++ src/index.js
@@ -1,32 +1,47 @@
 requirejs.config({
 	baseUrl: 'js',
 	paths: {
-		jquery: '../libs/jquery-1.11.1.min'
+		jquery: '../libs/zepto.min',
+		underscore: '../libs/underscore-min',
+		backbone: '../libs/backbone-min'
+	},
+	shim: {
+		jquery: {
+			exports: '$'
+		},
+		underscore: {
+			exports: '_'
+		},
+		backbone: {
+			deps: ['jquery', 'underscore'],
+			exports: 'Backbone'
+		}
 	},
 	waitSeconds: 15
+})(['backbone'], function(Backbone) {
+	if(typeof global != 'undefined') {
+		define(['Game', 'awt/KeyEvent'], function(Game, KeyEvent) {
+			new Game();
+			var gui = require('nw.gui');
+			var win = gui.Window.get();
+			window.onkeydown = function(event) {
+				switch (event.keyCode) {
+					case KeyEvent.KEY_F5:
+						win.reload(); break;
+					case KeyEvent.KEY_F11:
+						win.toggleFullscreen(); break;
+					case KeyEvent.KEY_F12:
+						if (!win.isDevToolsOpen()) win.showDevTools();
+						else win.closeDevTools(); break;
+					default:
+						break;
+				}
+			};
+			win.show();
+		});
+	} else {
+		requirejs(['Game'], function(Game) {
+			new Game();
+		});
+	}
 });
-if(typeof global != 'undefined') {
-	define(['Game', 'awt/KeyEvent'], function(Game, KeyEvent) {
-		new Game();
-		var gui = require('nw.gui');
-		var win = gui.Window.get();
-		window.onkeydown = function(event) {
-			switch (event.keyCode) {
-				case KeyEvent.KEY_F5:
-					win.reload(); break;
-				case KeyEvent.KEY_F11:
-					win.toggleFullscreen(); break;
-				case KeyEvent.KEY_F12:
-					if (!win.isDevToolsOpen()) win.showDevTools();
-					else win.closeDevTools(); break;
-				default:
-					break;
-			}
-		};
-		win.show();
-	});
-} else {
-	requirejs(['Game'], function(Game) {
-		new Game();
-	});
-}
