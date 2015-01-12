7eea257d0b6674336610862c74ba8210a34096e5
[D] Pakchoi <pakchoidora@gmail.com>
2015-01-12 16:10:54 +0800

    fixed: something wrong with requriejs using relative path.

--- src/js/Game.js
+++ src/js/Game.js
@@ -8,12 +8,9 @@ define(['jquery', 'awt/Stage', 'lang/Node'], function($, Stage, Node) {
 	}
 
 	return Node.extend({
-		__stage: {},
-		__graphics: {},
-		__speed: 50,
-		constructor: function () {
-			Node.prototype.constructor.apply(this, arguments);
-		},
+		__stage__: {},
+		__graphics__: {},
+		__speed__: 50,
 		start: function () {
 			this.init();
 		},
@@ -21,9 +18,9 @@ define(['jquery', 'awt/Stage', 'lang/Node'], function($, Stage, Node) {
 			this.render();
 		},
 		render: function () {
-			$(this.__stage.scene).attr({
-				width: this.__stage.width(),
-				height: this.__stage.height()
+			$(this.__stage__.scene()).attr({
+				width: this.__stage__.width(),
+				height: this.__stage__.height()
 			});
 		},
 		init: function () {
@@ -35,12 +32,12 @@ define(['jquery', 'awt/Stage', 'lang/Node'], function($, Stage, Node) {
 					evt.preventDefault();
 				}
 			});
-			this.__stage = new Stage($('#stage'));
-			this.__graphics = this.__stage.graphics;
-			this.__interval = setInterval(this.update.bind(this), this.__speed);
+			this.__stage__ = new Stage($('#stage'));
+			this.__graphics__ = this.__stage__.graphics();
+			this.__interval__ = setInterval(this.update.bind(this), this.__speed__);
 		},
 		pause: function() {
-			clearInterval(this.__interval);
+			clearInterval(this.__interval__);
 		},
 		resume: function() {
 
